c713a286cc2416042764bd470597242f3c6d0632
lolmageap <ekxk1234@naver.com>
2025-08-17 16:33:03 +0900

    feat: add Ktor RPC client module and configure Koin for dependency injection

--- server/src/main/kotlin/plugin/Frameworks.kt
+++ server/src/main/kotlin/plugin/Frameworks.kt
@@ -1,17 +1,16 @@
 package com.example.plugin
 
+import com.example.plugin.module.ktorRpcClientModule
+import com.example.plugin.module.weatherModule
 import io.ktor.server.application.*
 import kotlinx.rpc.krpc.ktor.server.Krpc
-import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureFrameworks() {
     install(Koin) {
         slf4jLogger()
-        modules(
-            module {}
-        )
+        modules(ktorRpcClientModule, weatherModule)
     }
     install(Krpc)
 }
---
+++ server/src/main/kotlin/plugin/module/KtorRpcClient.kt
@@ -0,0 +1,34 @@
+package com.example.plugin.module
+
+import Environment
+import io.ktor.client.*
+import io.ktor.http.*
+import kotlinx.rpc.krpc.ktor.client.KtorRpcClient
+import kotlinx.rpc.krpc.ktor.client.installKrpc
+import kotlinx.rpc.krpc.ktor.client.rpc
+import kotlinx.rpc.krpc.ktor.client.rpcConfig
+import kotlinx.rpc.krpc.serialization.json.json
+import org.koin.dsl.module
+
+
+val ktorRpcClientModule = module {
+    single<KtorRpcClient> {
+        HttpClient {
+            installKrpc {
+                waitForServices = true
+            }
+        }.rpc {
+            url {
+                host = Environment.HOST
+                port = Environment.PORT
+                encodedPath = Environment.ENCODED_PATH
+            }
+
+            rpcConfig {
+                serialization {
+                    json()
+                }
+            }
+        }
+    }
+}
